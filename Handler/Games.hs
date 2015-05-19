9532fa92120fb4f4224eb307193aeb8a0c7d3608
Trinh Tuan Phuong <lolovn@gmail.com>
2015-05-19 15:06:35 +0000

    Add command for getting single game

--- Handler/Games.hs
+++ Handler/Games.hs
@@ -6,17 +6,23 @@ import Import
 getGamesR :: Handler Html
 getGamesR = do
     defaultLayout $ do
-        aDomId <- newIdent
+        --aDomId <- newIdent
+        setTitle "Games list"
+        $(widgetFile "games")
+
+getGameR :: GameId -> Handler Html
+getGameR gameId = do
+    defaultLayout $ do
+        --aDomId <- newIdent
         setTitle "Games list"
         $(widgetFile "games")
 
 postGamesR :: Handler TypedContent
 postGamesR = do
      Just uid <- maybeAuthId
-     $(logInfo) "parsed submitted game"
      submittedGame <- requireJsonBody :: Handler Game
      let game = submittedGame { gameCreator = uid }
-     gameId <- runDB $ do
+     _ <- runDB $ do
         insert game
      selectRep $ do
         provideRep $ return $ toJSON game
